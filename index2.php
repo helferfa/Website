4c15d20b336e676f6b06c0ed8d03ba74886f6c2c
Fabian Helfer <helferfa@gmail.com>
2017-12-20 18:19:59 +0100

    added index.html and index.php to rep.

---
+++ index2.php
@@ -0,0 +1,31 @@
+
+<!DOCTYPE html>
+<html>
+<!--<?php/*
+	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
+		$uri = 'https://';
+	} else {
+		$uri = 'http://';
+	}
+	$uri .= $_SERVER['HTTP_HOST'];
+	header('Location: '.$uri.'/dashboard/');
+	exit;
+*/?>
+Something is wrong with the XAMPP installation :-( -->
+<head>
+	<title>HUB</title>
+	<style> 
+		body { background-image: url("http://cdn30.us1.fansshare.com/image/backgroundimage/hd-wallpapers-black-background-hd-image-website-for-website-527805647.jpg");
+	}
+</style>
+</head>
+<body>
+<div align="center">
+	<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+	<a href="Uni/index.php"><img src="https://lh4.googleusercontent.com/-vWnhjKrYsds/AAAAAAAAAAI/AAAAAAAAGIc/ILdHSLD9TcM/photo.jpg" width="120" height="120" border="0" alt="Home"></a>
+		&nbsp; &nbsp; &nbsp; &nbsp;
+		<a href="test/index.php"><img src="https://lh4.googleusercontent.com/-AEerXPqHm3M/AAAAAAAAAAI/AAAAAAAAGIg/nBpcOtv0Bxg/s0-c-k-no-ns/photo.jpg" width="120" height="120" border="0" alt="Home"></a>
+	</div>
+</body>
+</html>
+
